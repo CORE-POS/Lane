0c1096a977ad381c67fe0ffae9d1e09f9cfbf4c2
Andy Theuninck <andy@gohanman.com>
2017-01-22 16:41:54 -0600

    Handle invalid date format exceptions
    Conflicts:
    	pos/is4c-nf/lib/PrehLib.php

--- pos/is4c-nf/lib/PrehLib.php
+++ pos/is4c-nf/lib/PrehLib.php
@@ -551,9 +551,13 @@ public static function ageCheck($requiredAge, $ret)
     if (CoreLocal::get("memAge")=="") {
         CoreLocal::set("memAge",date('Ymd'));
     }
-    $stamp = strtotime(CoreLocal::get("memAge"));
-    $ofAgeOnDay = mktime(0, 0, 0, date('n', $stamp), date('j', $stamp), date('Y', $stamp) + $requiredAge);
-    $today = strtotime( date('Y-m-d') );
+    try {
+        $ofAgeOnDay = new DateTime(CoreLocal::get('memAge'));
+        $ofAgeOnDay->add(new DateInterval("P{$requiredAge}Y"));
+    } catch (Exception $ex) {
+        $ofAgeOnDay = new DateTime(date('Y-m-d'));
+    }
+    $today = new DateTime(date('Y-m-d'));
     if ($ofAgeOnDay > $today) {
         $ret['udpmsg'] = 'twoPairs';
         $ret['main_frame'] = $myUurl.'gui-modules/requestInfo.php?class=COREPOS-pos-parser-parse-UPC';
